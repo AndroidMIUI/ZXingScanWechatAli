83a2ea2f5ed31c73b610190e7375fa3eee9625d8
zhuxiangfeng <461952752@qq.com>
2020-07-16 14:14:35 +0800

    Initial commit

---
+++ app/src/main/java/com/library/ercode/MainActivity.java
@@ -0,0 +1,43 @@
+package com.library.ercode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button defaultStart = (Button) findViewById(R.id.default_start);
+        defaultStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DefaultCaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button weStart = (Button) findViewById(R.id.wechat_start);
+        weStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeChatCaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button aliStart = (Button) findViewById(R.id.alipay_start);
+        aliStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AliCaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
